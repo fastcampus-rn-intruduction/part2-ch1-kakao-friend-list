287c6651bbb11abd013cbd5ad02ea912519aa76f
daheeahn <deg9810@gmail.com>
2022-10-28 17:43:35 +0900

    [ch1 - 3 - UI 개발 (1) 헤더, SafeAreaView와 react-native-iphone-x-helper, @expo/vector-icon 사용법] 최종 코드

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import Header from "./src/Header";
+
+const statusBarHeight = getStatusBarHeight(true);
+console.log('statusBarHeight', statusBarHeight, Platform.OS)
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
     </View>
   );
 }
@@ -12,5 +18,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    paddingTop: statusBarHeight,
   },
 });
